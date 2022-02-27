ff305998fe2300cd8d78d691c5ed869f1ecefdad
Kim Chae Eun <kce3615@naver.com>
2022-02-27 22:12:17 +0900

    add: login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./routes/Login";
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
