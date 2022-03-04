6ad293dbb655982b6d39cda264930ea7a73f1f2d
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-03-04 22:49:38 +0900

    Feat: Add login feature

---
+++ src/main/java/promisor/promisor/domain/store/domain/Store.java
@@ -0,0 +1,9 @@
+package promisor.promisor.domain.store.domain;
+
+import promisor.promisor.domain.model.BaseEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Store extends BaseEntity {
+}
--- src/main/java/promisor/promisor/global/config/WebSecurityConfig.java
+++ src/main/java/promisor/promisor/global/config/WebSecurityConfig.java
@@ -3,13 +3,19 @@ package promisor.promisor.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import promisor.promisor.domain.member.MemberService;
+import promisor.promisor.global.config.security.CustomAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,14 +27,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/login");
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/members/**")
+                    .antMatchers("/", "/members/**", "/login/**")
                     .permitAll()
                 .anyRequest()
                 .authenticated().and()
-                .formLogin();
+                .formLogin().disable();
+
+
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.addFilterAt(getAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    protected CustomAuthenticationFilter getAuthenticationFilter() throws Exception {
+
+        AuthenticationManager authenticationManager = null;
+        CustomAuthenticationFilter authFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        try {
+            authFilter.setFilterProcessesUrl("/login");
+            authFilter.setAuthenticationManager(this.authenticationManagerBean());
+            authFilter.setUsernameParameter("email");
+            authFilter.setPasswordParameter("password");
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        return authFilter;
     }
 
     @Override
@@ -45,4 +73,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setUserDetailsService(memberService);
         return provider;
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
