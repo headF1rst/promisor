03c37fafd015bd7b6448e2eaa8f9107bff5910b8
Kim Chae Eun <kce3615@naver.com>
2022-04-04 22:12:30 +0900

    fix: Header 로고 추가

--- client/src/atoms/Logo.tsx
+++ client/src/atoms/Logo.tsx
@@ -1,18 +1,23 @@
 import styled from "styled-components";
 interface ILogo {
   value: string;
+  size?: number;
   onClick?: React.MouseEventHandler;
 }
-export const Logo = ({ value, onClick }: ILogo) => {
-  return <SLogo onClick={onClick}>{value}</SLogo>;
+export const Logo = ({ size, value, onClick }: ILogo) => {
+  return (
+    <SLogo size={size} onClick={onClick}>
+      {value}
+    </SLogo>
+  );
 };
 
-const SLogo = styled.div`
+const SLogo = styled.div<{ size?: number }>`
   display: flex;
   justify-content: center;
   align-items: center;
   font-family: winkle !important;
-  font-size: 3rem;
+  font-size: 2.3em;
   color: ${(p) => p.theme.green};
   text-shadow: -2px 1px 0px #ffffff;
   margin-block: 0.5em;
