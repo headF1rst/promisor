229e0e29253b867c29278ebbda36832e870d76b5
Hwang Seunghwan <xx0hnhwang@gmail.com>
2022-04-14 17:29:32 +0900

    Feat: 팀명 변경 기능 수정(#101)

--- src/main/java/promisor/promisor/domain/team/dto/EditTeamDto.java
+++ src/main/java/promisor/promisor/domain/team/dto/EditTeamDto.java
@@ -8,5 +8,6 @@ import javax.validation.constraints.NotBlank;
 public class EditTeamDto {
     @NotBlank(message = "새로운 그룹명을 입력해주세요.")
     private String groupName;
+    @NotBlank(message = "수정할 그룹의 id를 입력해주세요.")
     private Long groupId;
 }
