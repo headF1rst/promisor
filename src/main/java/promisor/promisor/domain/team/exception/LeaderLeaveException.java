812d4fb7234db359c99b41bdf0337f7049cdb928
juy4556 <kws05050@naver.com>
2022-04-14 23:54:40 +0900

    feat:add exception of leaving group

--- src/main/java/promisor/promisor/domain/team/exception/LeaderLeaveException.java
+++ src/main/java/promisor/promisor/domain/team/exception/LeaderLeaveException.java
@@ -1,2 +1,9 @@
-package promisor.promisor.domain.team.exception;public class LeaderLeaveException {
+package promisor.promisor.domain.team.exception;
+
+import promisor.promisor.global.error.ErrorCode;
+import promisor.promisor.global.error.exception.InvalidValueException;
+
+public class LeaderLeaveException extends InvalidValueException {
+    public LeaderLeaveException() {super(ErrorCode.ACCESS_DENIED);}
 }
+
--- src/main/java/promisor/promisor/domain/team/exception/TeamIdNotFoundException.java
+++ src/main/java/promisor/promisor/domain/team/exception/TeamIdNotFoundException.java
@@ -4,6 +4,6 @@ package promisor.promisor.domain.team.exception;
 import promisor.promisor.global.error.ErrorCode;
 import promisor.promisor.global.error.exception.EntityNotFoundException;
 
-public class TeamIdNotFound extends EntityNotFoundException {
-    public TeamIdNotFound() {super(ErrorCode.GROUP_ID_NOT_FOUND);}
+public class TeamIdNotFoundException extends EntityNotFoundException {
+    public TeamIdNotFoundException() {super(ErrorCode.GROUP_ID_NOT_FOUND);}
 }
