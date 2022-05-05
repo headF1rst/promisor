77e4c7c47598399b635ed5cbe53f74dad77482a5
Kim Chae Eun <kce3615@naver.com>
2022-05-05 16:44:49 +0900

    Feat: Delete date recommend list (#141)

--- client/src/pages/PromiseDate.tsx
+++ client/src/pages/PromiseDate.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BoxInput } from "../styles/Input";
 import PromiseDateCalendar from "../organisms/PromiseDateCalendar";
-import PromiseDateRecommendation from "../organisms/PromiseDateRecommendation";
 
 function PromiseDate() {
   return (
     <>
       <BoxInput type="date" />
       <PromiseDateCalendar />
-      <PromiseDateRecommendation />
     </>
   );
 }
