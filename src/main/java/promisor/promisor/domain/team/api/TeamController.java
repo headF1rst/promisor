7c827003c30f005bc781f32c80965e964020d551
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-05-15 12:53:20 +0900

    Fix: Fix rate limiting, createGroup return id

--- src/main/java/promisor/promisor/domain/team/api/TeamController.java
+++ src/main/java/promisor/promisor/domain/team/api/TeamController.java
@@ -5,14 +5,12 @@ import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Refill;
 import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import promisor.promisor.domain.team.dto.*;
 import promisor.promisor.domain.team.exception.TooManyRequestException;
 import promisor.promisor.domain.team.service.TeamService;
 import promisor.promisor.global.auth.JwtAuth;
-import promisor.promisor.global.token.exception.InvalidTokenException;
 
 
 import javax.validation.Valid;
@@ -21,32 +19,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/groups")
-@RequiredArgsConstructor
 public class TeamController {
 
     private final TeamService teamService;
+    private final Bucket bucket;
 
-    private Bucket bucket;
 
-    public void BucketController() {
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
 
         Bandwidth limit = Bandwidth.classic(5, Refill.intervally(5, Duration.ofMillis(30000)));
         this.bucket = Bucket.builder()
                 .addLimit(limit)
                 .build();
-    }
+        }
 
     @Operation(summary = "Create group", description = "그룹 생성")
     @PostMapping
-    public ResponseEntity<Void> createGroup(@JwtAuth String email,
+    public ResponseEntity<Long> createGroup(@JwtAuth String email,
                                             @RequestBody @Valid final CreateTeamDto request) {
 
         if (bucket.tryConsume(1)) {
-            teamService.createGroup(email, request);
+            return ResponseEntity.ok().body(teamService.createGroup(email, request));
         } else {
             throw new TooManyRequestException();
         }
-        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "Edit group name", description = "그룹 이름 수정")
