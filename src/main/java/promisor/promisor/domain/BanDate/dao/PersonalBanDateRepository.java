34823ee887797eb722e6f588f7f0c2756367f9bc
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-05-24 10:55:12 +0900

    Fix: Fix BanDate Directory name

--- src/main/java/promisor/promisor/domain/BanDate/dao/PersonalBanDateRepository.java
+++ src/main/java/promisor/promisor/domain/BanDate/dao/PersonalBanDateRepository.java
@@ -1,10 +1,9 @@
-package promisor.promisor.domain.BanDate.dao;
+package promisor.promisor.domain.bandate.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
-import promisor.promisor.domain.BanDate.domain.PersonalBanDate;
+import promisor.promisor.domain.bandate.domain.PersonalBanDate;
 
 @Transactional(readOnly = true)
 public interface PersonalBanDateRepository extends JpaRepository<PersonalBanDate, Long> {
-
 }
