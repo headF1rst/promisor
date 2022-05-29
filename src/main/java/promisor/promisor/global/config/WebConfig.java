ce480ace2880bc40b248d12a30509d819a4cf8ca
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-05-29 21:14:03 +0900

    Add: Add Origins to resolve CORS

--- src/main/java/promisor/promisor/global/config/WebConfig.java
+++ src/main/java/promisor/promisor/global/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://promisor.site", "http://promisor.site");
+                .allowedOrigins("http://localhost:8080", "https://promisor.site", "http://promisor.site",
+                        "http://localhost:3000");
     }
 }
