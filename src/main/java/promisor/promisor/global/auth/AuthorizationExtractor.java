d885750b22ccfd5f343d6c891332255d9595f309
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-06-23 23:00:22 +0900

    Refactor: 메서드 분리를 통한 가독성 개선

--- src/main/java/promisor/promisor/global/auth/AuthorizationExtractor.java
+++ src/main/java/promisor/promisor/global/auth/AuthorizationExtractor.java
@@ -1,13 +1,10 @@
 package promisor.promisor.global.auth;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Slf4j
 public class AuthorizationExtractor {
 
@@ -15,23 +12,33 @@ public class AuthorizationExtractor {
     public static final String ACCESS_TOKEN_TYPE = AuthorizationExtractor.class.getSimpleName() + ".ACCESS_TOKEN_TYPE";
     public static final String BEARER_TYPE = "Bearer";
 
+    private AuthorizationExtractor() {}
+
     public static String extract(HttpServletRequest request) {
 
         Enumeration<String> headers = request.getHeaders(AUTHORIZATION);
         log.info("Header Message: '{}'", headers);
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
-            if (value.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
-                String authHeaderValue = value.substring(BEARER_TYPE.length()).trim();
-                request.setAttribute(ACCESS_TOKEN_TYPE,
-                        value.substring(0, BEARER_TYPE.length()).trim());
-                int commaIndex = authHeaderValue.indexOf(',');
-                if (commaIndex > 0) {
-                    authHeaderValue = authHeaderValue.substring(0, commaIndex);
-                }
-                return authHeaderValue;
-            }
+            return checkValueStartsWithBearer(request, value);
         }
         return null;
+    }
+
+    public static String checkValueStartsWithBearer(HttpServletRequest request, String value) {
+        if (value.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
+            String authHeaderValue = value.substring(BEARER_TYPE.length()).trim();
+            request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(0, BEARER_TYPE.length()).trim());
+            int commaIndex = authHeaderValue.indexOf(',');
+            return updateAuthHeaderValue(authHeaderValue, commaIndex);
+        }
+        return null;
+    }
+
+    public static String updateAuthHeaderValue(String authHeaderValue, int commaIndex) {
+        if (commaIndex > 0) {
+            authHeaderValue = authHeaderValue.substring(0, commaIndex);
+        }
+        return authHeaderValue;
     }
 }
