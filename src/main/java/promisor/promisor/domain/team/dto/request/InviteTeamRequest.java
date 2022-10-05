0546498e44cb989b6e8aa5a7981737c7f78a3682
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-10-06 00:54:12 +0900

    refactor: group을 team으로 네이밍 수정

--- src/main/java/promisor/promisor/domain/team/dto/request/InviteTeamRequest.java
+++ src/main/java/promisor/promisor/domain/team/dto/request/InviteTeamRequest.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class InviteTeamRequest {
     private Long[] memberId;
-    private Long groupId;
+    private Long teamId;
 }
--- src/main/java/promisor/promisor/domain/team/exception/TeamNotFoundException.java
+++ src/main/java/promisor/promisor/domain/team/exception/TeamNotFoundException.java
@@ -3,8 +3,8 @@ package promisor.promisor.domain.team.exception;
 import promisor.promisor.global.error.ErrorCode;
 import promisor.promisor.global.error.exception.EntityNotFoundException;
 
-public class GroupNotFoundException extends EntityNotFoundException {
-    public GroupNotFoundException() {
+public class TeamNotFoundException extends EntityNotFoundException {
+    public TeamNotFoundException() {
         super(ErrorCode.GROUP_EMPTY);
     }
 }
