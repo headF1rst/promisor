b081befb649b4ba2817e5f2bd3718d4707eef6ff
seungki <sysh9498@gmail.com>
2023-01-28 18:13:17 +0900

    feat: Make URL for kakao login

--- client/src/auth/OAuth.js
+++ client/src/auth/OAuth.js
@@ -1,4 +1,4 @@
-const REST_API_KEY = "b7a18cd68f31fdba3ce47cf47322c9a1";
-const REDIRECT_URI = "http://localhost:8000/oauth/kakao";
+export const REST_API_KEY = "b7a18cd68f31fdba3ce47cf47322c9a1";
+export const REDIRECT_URI = "http://localhost:3000/oauth/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
