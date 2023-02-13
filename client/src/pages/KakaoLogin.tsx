a3b6e85719185bdff577b8b47a6ec61cbb3d8a58
seungki <sysh9498@gmail.com>
2023-02-13 22:06:09 +0900

    feat: accesstoken 발급

--- client/src/pages/KakaoLogin.tsx
+++ client/src/pages/KakaoLogin.tsx
@@ -12,6 +12,17 @@ function KakaoLogin() {
     localStorage.setItem("CODE", KAKAO_CODE);
   };
 
+  const pushAccessToken = () => {
+    axios
+      .post("http://localhost:8000/oauth/kakao/login", {
+        accessToken: sessionStorage.getItem("kakao_access_token"),
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => console.log(error));
+  };
+
   useEffect(() => {
     let params = new URL(document.URL).searchParams;
     let KAKAO_CODE = params.get("code");
@@ -28,17 +39,23 @@ function KakaoLogin() {
         },
       )
       .then(res => {
-        axios
-          .post("http://localhost:8000/oauth/kakao/login", {
-            accessToken: res.data.access_token,
-          })
-          .then(res => {
-            console.log(res);
-          });
+        sessionStorage.setItem("kakao_access_token", res.data.access_token);
+        console.log(res);
       });
   }, []);
 
-  return <div>CODE: {KAKAO_CODE}</div>;
+  return (
+    <div>
+      CODE: {KAKAO_CODE}
+      <button
+        onClick={() => {
+          pushAccessToken();
+        }}
+      >
+        엑세스코드전송
+      </button>
+    </div>
+  );
 }
 
 export default KakaoLogin;
